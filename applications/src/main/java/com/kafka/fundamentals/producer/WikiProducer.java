2c53a4bb3013cf9125f0fea03737522b9bd36ade
Bar Snir <bar1991snir@gmail.com>
2024-01-16 16:00:33 +0200

    added some sections

--- applications/src/main/java/com/kafka/fundamentals/producer/WikiProducer.java
+++ applications/src/main/java/com/kafka/fundamentals/producer/WikiProducer.java
@@ -7,6 +7,8 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +21,18 @@ public class WikiProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Safety check
+        // properties.setProperty(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "3000");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "120000");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        // High throughput
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
 
         // create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
@@ -36,7 +50,5 @@ public class WikiProducer {
 
         // we produce for 10 minutes and block the program until then
         TimeUnit.MINUTES.sleep(10);
-
-
     }
 }
